49ed3b25e5f64a68672c425b3a8b81c5143d48bd
BlackChang <dnjs7651@gmail.com>
2020-04-08 12:23:31 +0900

    Make workspace

---
+++ seven_dwarf/seven_dwarf/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  seven_dwarf
+//
+//  Created by 이원창 on 2020/04/08.
+//  Copyright © 2020 이원창. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
