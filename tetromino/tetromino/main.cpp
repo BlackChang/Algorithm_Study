f57865b092826fe9d49144b23818949e1a3135ff
BlackChang <dnjs7651@gmail.com>
2020-04-19 22:38:36 +0900

    Second Try
    Add input condition(n >= 4, m <= 500)

--- tetromino/tetromino/main.cpp
+++ tetromino/tetromino/main.cpp
@@ -11,24 +11,28 @@
 #include <cstdio>
 
 int main(int argc, const char * argv[]) {
-    int m, n = 0;
+    int m = 0;
+    int n = 0;
     int max = 0;
     int temp = 0;
     int** arr;
-
-    scanf("%d %d", &m, &n);
-    arr = new int*[m];
-    for(int i = 0; i < m; i++)
-        arr[i] = new int[n];
     
-    for(int i = 0; i < m; i++){
+    scanf("%d %d", &n, &m);
+    while(n < 4 && (m > 500 || m <= 0)){
+        scanf("%d %d", &n, &m);
+    }
+    arr = new int*[m];
+    for(int i = 0; i < n; i++)
+        arr[i] = new int[m];
+    
+    for(int i = 0; i < n; i++){
         getchar();
-        for(int j = 0; j < n; j++)
+        for(int j = 0; j < m; j++)
             scanf("%d", &arr[i][j]);
     }
 
-    for(int i = 0; i < m - 1; i++){
-        for(int j = 0; j < n - 2; j++){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < m - 2; j++){
             temp = arr[i+1][j] + arr[i+1][j+1] + arr[i][j+1] + arr[i][j+2];
             if(max < temp)
                 max = temp;
@@ -49,8 +53,8 @@ int main(int argc, const char * argv[]) {
                 max = temp;
         }
     }
-    for(int i = 0; i < m - 2; i++){
-        for(int j = 0; j < n - 1; j++){
+    for(int i = 0; i < n - 2; i++){
+        for(int j = 0; j < m - 1; j++){
             temp = arr[i][j] + arr[i+1][j] + arr[i+1][j+1] + arr[i+2][j+1];
             if(max < temp)
                 max = temp;
@@ -71,22 +75,22 @@ int main(int argc, const char * argv[]) {
                 max = temp;
         }
     }
-    for(int i = 0; i < m; i++){
-        for(int j = 0; j < n - 3; j++){
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m - 3; j++){
             temp = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i][j+3];
             if(max < temp)
                 max = temp;
         }
     }
-    for(int i = 0; i < m - 3; i++){
-        for(int j = 0; j < n; j++){
+    for(int i = 0; i < n - 3; i++){
+        for(int j = 0; j < m; j++){
             temp = arr[i][j] + arr[i+1][j] + arr[i+2][j] + arr[i+3][j];
             if(max < temp)
                 max = temp;
         }
     }
-    for(int i = 0; i < m - 1; i++){
-        for(int j = 0; j < n - 1; j++){
+    for(int i = 0; i < n - 1; i++){
+        for(int j = 0; j < m - 1; j++){
             temp = arr[i][j] + arr[i+1][j] + arr[i+1][j+1] + arr[i][j+1];
             if(max < temp)
                 max = temp;
