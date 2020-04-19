8ad9d2eb1ecc1da8141c116838e08c099e51e71c
BlackChang <dnjs7651@gmail.com>
2020-04-19 14:52:17 +0900

    First Try

--- day_calculate/day_calculate/main.cpp
+++ day_calculate/day_calculate/main.cpp
@@ -7,9 +7,27 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <cstdio>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int e = 0;
+    int s = 0;
+    int m = 0;
+    int i = 0;
+    int temp = 0;
+    int year = 0;
+
+    scanf("%d %d %d", &e, &s, &m);
+    while(true){
+        temp = i * 28 + s;
+        if(((temp-e)%15 == 0) && ((temp-m)%19 == 0)){
+            year = temp;
+            break;
+        }
+        i += 1;
+    }
+    
+    printf("%d\n", year);
     return 0;
 }
