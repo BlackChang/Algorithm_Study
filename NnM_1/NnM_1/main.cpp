2d5b80e2fdc7d99c96c605f437d48d7adaccbedd
BlackChang <dnjs7651@gmail.com>
2020-04-27 16:51:04 +0900

    First Try

--- NnM_1/NnM_1/main.cpp
+++ NnM_1/NnM_1/main.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  NnM_2
+//  NnM_1
 //
 //  Created by 이원창 on 2020/04/25.
 //  Copyright © 2020 이원창. All rights reserved.
@@ -12,44 +12,36 @@
 int n = 0;
 int m = 0;
 int* arr;
+int* check;
 void makeSequence(int);
-int checkValid(int, int);
 int main(int argc, const char * argv[]) {
     scanf("%d %d", &n, &m);
     arr = new int[m];
-
-    for(int i = 0; i < m; i++)
+    check = new int[m];
+    
+    for(int i = 0; i < m; i++){
         arr[i] = 0;
-
-//    for(int i = 0; i < n; i++)
-        makeSequence(0);
-
+        check[i] = 0;
+    }
+    
+    makeSequence(0);
     return 0;
 }
 void makeSequence(int num){
-    for(int i = 1; i < n + 1; i++){
-        if(checkValid(i, num)){
+    if(num == m){
+        for(int i = 0; i < m; i++){
+            printf("%d ", arr[i]);
+        }
+        printf("\n");
+    }
+    
+    for(int i = 1; i <= n; i++){
+        if(check[i] == 0){
             arr[num] = i;
+            check[i] = 1;
             makeSequence(num + 1);
+            arr[num] = 0;
+            check[i] = 0;
         }
-    }
-    num = 0;
-    for(int i = 0; i < m; i++)
-        printf("%d ", arr[i]);
-    printf("\n");
-}
-int checkValid(int check, int num){
-    if(num == 0){
-        if(arr[0] != check)
-            return 1;
-        else
-            return 0;
-    }
-    else{
-        for(int i = 0; i < num; i++){
-            if(arr[i] == check)
-                return 0;
-        }
-        return 1;
     }
 }
