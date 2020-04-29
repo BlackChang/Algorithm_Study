6eb059569deb4ec48e35d24996a9c3d8fead90d9
BlackChang <dnjs7651@gmail.com>
2020-04-29 16:49:09 +0900

    First Try

--- dolls/dolls/main.cpp
+++ dolls/dolls/main.cpp
@@ -7,9 +7,52 @@
 //
 
 #include <iostream>
+#include <algorithm>
+#include <cmath>
 
+using namespace std;
+int* doll;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int n = 0;
+    int k = 0;
+    long double sum = 0;
+    long double avg = 0;
+    long double v = 0;
+    long double stdv = 0;
+    long double min = 0;
+    long double temp = 0;
+    
+    scanf("%d %d", &n, &k);
+    doll = new int[n];
+    min = 10000000;
+    
+    for(int i = 0; i < n; i++)
+        scanf("%d", &doll[i]);
+        
+    for(int i = 0; i < n - k + 1; i++){
+        temp = k;
+        while(i + temp <= n){
+            sum = 0;
+            avg = 0;
+            v = 0;
+            stdv = 0;
+            for(int j = i; j < i + temp; j++)
+                sum += doll[j];
+
+            avg = (sum / temp);
+
+            for(int j = i; j < i + temp; j++)
+                v += (long double)(pow((avg - doll[j]), 2));
+            
+            v /= temp;
+            stdv = (long double)sqrt(v);
+            if(stdv < min)
+                min = stdv;
+            
+            temp += 1;
+        }
+    }
+    printf("%Lf\n", min);
+    
     return 0;
 }
