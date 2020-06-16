7ca4c4135f9c03d56ce162123a431597e3bcedcb
BlackChang <dnjs7651@gmail.com>
2020-06-17 00:39:30 +0900

    Second Try

--- death_knight/death_knight/main.cpp
+++ death_knight/death_knight/main.cpp
@@ -9,19 +9,19 @@
 #include <iostream>
 #include <algorithm>
 #include <queue>
+#include <cstring>
 using namespace std;
-queue<pair<int, int>> horse;
-int** cnt;
-int n = 0;
-int r1 = 0;
-int c1 = 0;
-int r2 = 0;
-int c2 = 0;
-int r = 0;
-int w = 0;
-int newR[6] = {-2, -2, 0, 0, 2, 2};
-int newC[6] = {-1, 1, -2, 2, -1, 1};
 int main(int argc, const char * argv[]) {
+    queue<pair<int, int>> horse;
+    int** cnt;
+    int n = 0;
+    int r1 = 0;
+    int c1 = 0;
+    int r2 = 0;
+    int c2 = 0;
+    int newR[6] = {-2, -2, 0, 0, 2, 2};
+    int newC[6] = {-1, 1, -2, 2, -1, 1};
+
     scanf("%d", &n);
     scanf("%d %d %d %d", &r1, &c1, &r2, &c2);
     horse.push(make_pair(r1, c1));
@@ -30,6 +30,7 @@ int main(int argc, const char * argv[]) {
     for(int i = 0; i < n; i++)
         cnt[i] = new int[n];
     
+    memset(cnt, false, sizeof(cnt));
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++)
             cnt[i][j] = -1;
@@ -39,19 +40,19 @@ int main(int argc, const char * argv[]) {
     while (!horse.empty()) {
         int nR = 0;
         int nC = 0;
-        int R = horse.front().first;
-        int C = horse.front().second;
+        int r = horse.front().first;
+        int c = horse.front().second;
         horse.pop();
         for (int i = 0; i < 6; i++) {
-            nR = R + newR[i];
-            nC = C + newC[i];
+            nR = r + newR[i];
+            nC = c + newC[i];
             if (nR < 0 || nC >= n || nC < 0 || nR >= n)
                 continue;
             else {
-                if (cnt[nR][nC] != -1 && cnt[R][C] + 1 > cnt[nR][nC])
+                if (cnt[nR][nC] != -1 && cnt[r][c] + 1 > cnt[nR][nC])
                     continue;
                 
-                cnt[nR][nC] = cnt[R][C] + 1;
+                cnt[nR][nC] = cnt[r][c] + 1;
                 horse.push(make_pair(nR, nC));
             }
         }
