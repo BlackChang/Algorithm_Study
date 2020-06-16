2c4702becd90ea85b173f98e14f0155ecdee15be
BlackChang <dnjs7651@gmail.com>
2020-06-16 19:41:12 +0900

    New Project

---
+++ word_math/word_math/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  word_math
+//
+//  Created by 이원창 on 2020/06/15.
+//  Copyright © 2020 이원창. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int n = 0;
+int ans = 0;
+int r1, c1, r2, c2;
+int main(int argc, const char * argv[]) {
+    scanf("%d", &n);
+    scanf("%d %d %d %d", &r1, &c1, &r2, &c2);
+    
+    
+    printf("%d\n", ans);
+    return 0;
+}
