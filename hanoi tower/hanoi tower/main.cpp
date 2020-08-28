9b3dd54290fcc715d5ab5326f32023e110729f47
BlackChang <dnjs7651@gmail.com>
2020-08-28 15:09:37 +0900

    First Try

--- hanoi tower/hanoi tower/main.cpp
+++ hanoi tower/hanoi tower/main.cpp
@@ -7,9 +7,35 @@
 //
 
 #include <iostream>
+#include <cmath>
 
+using namespace std;
+int n = 0;
+int tmp = 0;
+void hanoi(int, int, int, int);
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cin.tie(NULL);
+    cin.sync_with_stdio(false);
+    cin >> n;
+    
+    cout << (int)(pow(2, n) - 1)<< endl;
+    hanoi(n, 1, 2, 3);
+
     return 0;
+}
+void hanoi(int n, int start, int tmp ,int end){
+    if(n == 1){
+        cout << "1 3" << endl;
+        return;
+    }
+    else if(n == 2){
+        cout << start << ' ' << tmp << endl;
+        cout << start << ' ' << end << endl;
+        cout << tmp << ' ' << end << endl;
+        return;
+    }
+
+    hanoi(n - 1, start, end, tmp);
+    cout << start << ' ' << end << endl;
+    hanoi(n - 1, tmp, start, end);
 }
