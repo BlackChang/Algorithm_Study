9f8e04450280e20233a87d6037bb8b79eef7879a
BlackChang <dnjs7651@gmail.com>
2020-08-28 15:46:57 +0900

    Succeed

--- hanoi tower/hanoi tower/main.cpp
+++ hanoi tower/hanoi tower/main.cpp
@@ -7,40 +7,32 @@
 //
 
 #include <iostream>
-#include <cmath>
-
 using namespace std;
-int tmp = 0;
 void hanoi(int, int, int, int);
 int main(int argc, const char * argv[]) {
     int n = 0;
     long long ans = 1;
     
     cin.tie(NULL);
-    cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    cin >> n;
     
+    cin >> n;
     for(int i = 0; i < n; i++){
         ans *= 2;
     }
     ans -= 1;
     
-    cout << ans<< endl;
+    cout << ans << "\n";
     hanoi(n, 1, 2, 3);
 
     return 0;
 }
 void hanoi(int n, int start, int mid ,int end){
-    if(n == 1){
-        cout << start << " " << end << endl;
-        return;
-    }
-    else{
+    if(n == 1)
+        cout << start << " " << end << "\n";
+    else {
         hanoi(n - 1, start, end, mid);
-        cout << start << " " << end << endl;
+        hanoi(1, start, mid, end);
         hanoi(n - 1, mid, start, end);
-        return;
     }
-
 }
