d8045cd09541bcefa485098cc1c70b627bb753ed
BlackChang <dnjs7651@gmail.com>
2020-08-30 16:15:49 +0900

    First Try

--- link number_1790/link number_1790/main.cpp
+++ link number_1790/link number_1790/main.cpp
@@ -7,9 +7,62 @@
 //
 
 #include <iostream>
-
+#include <cmath>
+using namespace std;
+int n = 0;
+int k = 0;
+void dfs(int, int, int);
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    cin >> n >> k;
+
+    int tmp = n;
+    int nLen = 0;
+    int totalLen = 0;
+
+    while(true){
+        if(tmp <= 0)
+            break;
+        tmp = tmp / 10;
+        nLen++;
+    }
+
+    for(int i = 0; i < nLen; i++){
+        totalLen += n;
+        totalLen -= pow(10, i) - 1;
+    }
+    
+    if(k > totalLen)
+        cout << "-1\n";
+    else{
+        if(k < 10)
+            cout << k << endl;
+        else
+            dfs(k, 10, 1);
+    }
+    
     return 0;
 }
+void dfs(int num, int cn, int level){
+    if (num - (cn - cn / 10) * level > 0)
+        dfs(num - (cn - cn / 10) * level, cn * 10, level + 1);
+    int a = num / level;
+    int b = num % level;
+    cn /= 10;
+    int tmp = cn + a - 1;
+    int answer = tmp % 10;
+    if (b > 0){
+        tmp++;
+        if (tmp > n)
+            return;
+        for (int i = 0; i < b; i++){
+            answer = tmp / cn;
+            tmp %= cn;
+            cn /= 10;
+        }
+    }
+    else if (tmp > n)
+        return;
+
+    cout << answer << endl;
+}
+
