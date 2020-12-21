9d851befb467058180af540c9228df28032e3024
BlackChang <dnjs7651@gmail.com>
2020-12-21 16:23:03 +0900

    success

--- chicken_halfNhalf/chicken_halfNhalf/main.cpp
+++ chicken_halfNhalf/chicken_halfNhalf/main.cpp
@@ -6,9 +6,34 @@
 //
 
 #include <iostream>
-
+using namespace std;
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int a = 0;
+    int b = 0;
+    int c = 0;
+    int x = 0;
+    int y = 0;
+    int result = 0;
+    int temp = 0;
+    
+    cin >> a >> b >> c >> x >> y;
+    result = a * x + b * y;
+    
+    if(x < y){
+        if(b > 2 * c)
+            temp = 2 * y * c;
+        else
+            temp = 2 * c * x + (y - x) * b;
+    }
+    else{
+        if(a > 2 * c)
+            temp = 2 * x * c;
+        else
+            temp = 2 * c * y + (x - y) * a;
+    }
+    if(result > temp)
+        result = temp;
+
+    cout << result << endl;
     return 0;
 }
